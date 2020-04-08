f39d108e861bdaebac36a42dc8b3033628c174f2
Ümit UZ <umituz998@gmail.com>
2020-04-08 16:10:52 +0300

    CORS Middleware eklendi

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
+Route::group(['middleware' => 'api'], function ($router) {
 
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
